ed8ba0d13ab8597717098db848dbf844607e1b40
ScottDenton <scottyd_14@hotmail.com>
2019-04-08 16:26:52 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1> Movies Page</h1>
+        {movies.map(movie => (
+          <div>
+            <h4>Title: {movie.title}</h4>
+            <p>Time: {movie.time}</p>
+            <ul>
+              {movie.genres.map(genre => (
+                <li> {genre}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,33 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+  const style = {color: 'white', background: "blue" }
   return (
-    <div>
-      {/*{code here}*/}
+    <div className ='navbar'>
+      <NavLink
+        to='/'
+        exact
+        activeStyle={style}
+        >Home
+      </NavLink>
+      <NavLink
+        to='/movies'
+        exact
+        activeStyle={style}
+        >Movies
+      </NavLink>
+      <NavLink
+        to='/directors'
+        exact
+        activeStyle={style}
+        >Directors
+      </NavLink>
+      <NavLink
+        to='/actors'
+        exact
+        activeStyle={style}
+        >Actors
+      </NavLink>
     </div>
   );
 };
