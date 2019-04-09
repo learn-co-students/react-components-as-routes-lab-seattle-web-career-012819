42edd5ef8188243bba5e777dcc9be570cb739290
Emily Wagner <EmilyWagner598@gmail.com>
2019-04-08 17:25:04 -0700

    Done

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { directors } from '../data';
 
-const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+class Directors extends Component {
+  loadDirectors = () => {
+    return (directors.map((dir, index) => (
+      <div key={index}>
+        <h2>Name: {dir.name}</h2>
+        <p>Movies: </p>
+        <ul>
+          {dir.movies.map((mov, index) => (
+            <li key={index}>{mov}</li>
+          ))}
+        </ul>
+      </div>
+    )))
+  }
+  
+  render() {
+    return(
+      <div>
+        <h1>Directors Page</h1>
+        {this.loadDirectors()}
+      </div>
+    )
+  }
 }
 
 export default Directors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { movies } from '../data';
 
-const Movies = () => {
-  return (
-    <div>
-        {/*{code here}*/}
-    </div>
-  );
-};
+class Movies extends Component {
+  loadMovies = () => {
+    return(movies.map((movie, index) => (
+      <div key={index}>
+        <h2>Name: {movie.title}</h2>
+        <p>Time: {movie.time} minutes</p>
+        <p>Genres: </p>
+        <ul>
+          {movie.genres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+          ))}
+        </ul>
+      </div>
+    )))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Movies Page</h1>
+        {this.loadMovies()}
+      </div>
+    )
+  }
+}
 
 export default Movies;
